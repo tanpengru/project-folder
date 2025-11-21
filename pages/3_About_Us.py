dc83005745b7b4fb72774767df76f5656812b665
tanpengru <tanpengru@gmail.com>
2025-11-21 17:17:50 +0800

    update to about, password and requirements

--- pages/3_About_Us.py
+++ pages/3_About_Us.py
@@ -11,7 +11,9 @@ st.title("About this App")
 
 st.write("This is a Research App can 1) Upload & Summarize Research Papers 2) Search Open Access Papers.")
 
-with st.expander("Upload & Aummarize Research Papers"):
+with st.expander("Upload & Summarize Research Papers"):
     st.write("1. Browse and Upload Research Papers.")
     st.write("2. Question the research paper.")
-    st.write("3. The app will generate a text completion based on your prompt.")
+with st.expander("Open Access Search"):
+    st.write("1. Search open access database.")
+    st.write("2. Click Submit.")
